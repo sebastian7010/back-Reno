75dd6749c7baec9305545f6e52302e2c5a4607d1
“sebastian7010” <sebastianssc01@gmial.com>
2024-11-05 19:49:40 -0500

    arreglo de la advertencia roja de mongodb

--- index.js
+++ index.js
@@ -14,10 +14,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 // Conectar a MongoDB
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
+mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log("Estamos conectados a la base de datos, ¡soy genial!"))
     .catch((error) => console.error("Error al conectar a MongoDB:", error));
 
